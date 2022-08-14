b5ebea1b3584997162ad2b59d452be256e9a0e2a
Guillaume Fradin <guillaumefradin@gmail.com>
2022-08-14 12:06:40 +0100

    update layout

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,22 @@
 <template>
   <div
-    class="h-full relative items-center justify-center w-full overflow-x-hidden lg:pt-40 lg:pb-40 xl:pt-40 xl:pb-64"
+    class="h-full relative items-center justify-center w-full overflow-x-hidden"
   >
-    <div
-      class="container flex flex-col items-center justify-between h-full max-w-6xl px-8 mx-auto -mt-32 lg:flex-row xl:px-0"
-    >
-      <div
-        class="z-30 flex flex-col items-center w-full max-w-xl pt-48 text-center lg:items-start lg:w-1/2 lg:pt-20 xl:pt-40 lg:text-left"
-      >
-        <h1
-          class="relative mb-4 text-3xl font-black leading-tight text-gray-900 sm:text-6xl xl:mb-8"
-        >
-          Build the Next Great Thing
-        </h1>
-        <p
-          class="pr-0 mb-8 text-base text-gray-600 sm:text-lg xl:text-xl lg:pr-20"
-        >
-          Are you ready to start your adventure and start building the next
-          greatest SAAS app on the market?
-        </p>
-        <a
-          href="#_"
-          class="relative self-start inline-block w-auto px-8 py-4 mx-auto mt-0 text-base font-bold text-white bg-indigo-600 border-t border-gray-200 rounded-md shadow-xl sm:mt-1 fold-bold lg:mx-0"
-          >Signup Today!</a
-        >
-        <!-- Integrates with section -->
-      </div>
-      <div
-        class="relative z-50 flex flex-col items-end justify-center w-full h-full lg:w-1/2 ms:pl-10"
-      >
-        <div
-          class="container relative left-0 w-full max-w-4xl lg:absolute xl:max-w-6xl lg:w-screen"
-        >
-          <img
-            src="https://cdn.devdojo.com/images/september2020/macbook-mockup.png"
-            class="w-full h-auto mt-20 mb-20 ml-0 lg:mt-24 xl:mt-40 lg:mb-0 lg:h-full lg:-ml-12"
-          />
-        </div>
-      </div>
+    <div class="h-full items-center justify-between flex flex-col lg:flex-row">
+      <HeroText></HeroText>
+      <HeroVisuals></HeroVisuals>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import HeroText from "./components/HeroText.vue";
+import HeroVisuals from "./components/HeroVisuals.vue";
 
 export default defineComponent({
   name: "App",
+  components: { HeroText, HeroVisuals },
 });
 </script>
 
---
+++ src/components/HeroVisuals.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex-1">
+    <h1
+      class="relative mb-4 text-2xl font-black leading-tight text-gray-900 sm:text-6xl xl:mb-8"
+    >
+      Visuals
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Hero-Visuals",
+});
+</script>
