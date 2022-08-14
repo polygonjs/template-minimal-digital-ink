db311b0a19f7693a9fedde9e0fb93542f87702c4
Guillaume Fradin <guillaumefradin@gmail.com>
2022-08-14 11:31:49 +0100

    setup tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -8,7 +9,6 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
